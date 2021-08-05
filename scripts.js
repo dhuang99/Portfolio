f3f1dacda52eb80f038fc5dd316898407c9a62db
Daniel Huang <danielhuang99@gmail.com>
2021-08-05 14:57:22 -0400

    Organized scripts

--- scripts.js
+++ scripts.js
@@ -1,21 +1,39 @@
+//WOW Initialization
 new WOW().init();
 
 // Change style of navbar on scroll
 window.onscroll = function() {
-    myFunction()
+    var fileName = location.pathname.split("/").slice(-1);
+    if (fileName == "index.html") {
+        home()
+    } else {
+        other()
+    }
 };
 
-function myFunction() {
+function home() {
+
     var navbar = document.getElementById("myNavbar");
     if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " primary-color-scheme" + " secondary-color-scheme";
+    } else {
+        navbar.className = navbar.className.replace(" w3-card w3-animate-top primary-color-scheme", "");
+    }
+}
+
+function other() {
+
+    var navbar = document.getElementById("myNavbar");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " primary-color-scheme";
     } else {
         navbar.className = navbar.className.replace(" w3-card w3-animate-top primary-color-scheme", "");
     }
 }
 
-// Used to toggle the menu on small screens when clicking on the menu button
-function toggleFunction() {
+
+// Toggles mini-menu
+function toggleMiniMenu() {
     var x = document.getElementById("navDemo");
     if (x.className.indexOf("w3-show") == -1) {
         x.className += " w3-show";
