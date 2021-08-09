304f9548c6116a38ced88bd34c826b030e1dcf40
Daniel Huang <danielhuang99@gmail.com>
2021-08-09 17:04:05 -0400

    Updated menu bar and mini menu bar

--- scripts.js
+++ scripts.js
@@ -15,9 +15,9 @@ function home() {
 
     var navbar = document.getElementById("myNavbar");
     if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-        navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " primary-color-scheme" + " secondary-color-scheme";
+        navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " primary-color-scheme" + "  transparent75" + " secondary-color-scheme";
     } else {
-        navbar.className = navbar.className.replace(" w3-card w3-animate-top primary-color-scheme", "");
+        navbar.className = navbar.className.replace(" w3-card w3-animate-top primary-color-scheme  transparent75", "");
     }
 }
 
@@ -25,7 +25,7 @@ function other() {
 
     var navbar = document.getElementById("myNavbar");
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " primary-color-scheme" + " transparent75";
+        navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " primary-color-scheme" + "  transparent75";
     } else {
         navbar.className = navbar.className.replace(" w3-card w3-animate-top primary-color-scheme ", "");
     }
@@ -35,9 +35,18 @@ function other() {
 // Toggles mini-menu
 function toggleMiniMenu() {
     var x = document.getElementById("navmenu");
-    if (x.className.indexOf("w3-show") == -1) {
-        x.className += " w3-show";
+    if (x.className.indexOf("navmenu-show") == -1) {
+        x.className += " navmenu-show";
     } else {
-        x.className = x.className.replace(" w3-show", "");
+        x.className = x.className.replace(" navmenu-show", "");
     }
 }
+
+
+window.addEventListener('scroll', function() {
+    var x = document.getElementById("navmenu")
+    if (x.classList.contains("navmenu-show")) {
+        console.log('testing')
+        x.className = x.className.replace("navmenu-show", "");
+    }
+});
